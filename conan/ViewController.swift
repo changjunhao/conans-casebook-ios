3e1074b43145a74c9ba4dd7f0b99496ef0223ca0
思尘 <ward_1992@msn.cn>
2020-06-22 19:29:20 +0800

    update: 首页按钮

--- conan/ViewController.swift
+++ conan/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
-    var currentIndex: Int = 0
     var caseCardViewControllers: [UIViewController] = []
+    var button: UIButton?
+    var caseBookList: [CaseBook]?
+    var currentIndex: Int = 0 {
+        didSet {
+            button?.setTitle(caseBookList![currentIndex].waiting ? "静候上线" : "精彩呈现", for: .normal)
+        }
+    }
+    
+    var nextIndex: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,15 +39,33 @@ class ViewController: UIViewController, UIPageViewControllerDelegate, UIPageView
         view.addSubview(pageViewController.view)
         pageViewController.didMove(toParent: self)
 
-        var caseBookList: [CaseBook] = []
+        caseBookList = [CaseBook]()
         for i in 0..<21 {
-            caseBookList.append(CaseBook.create(index: i))
+            caseBookList!.append(CaseBook.create(index: i))
         }
-        for (_, item) in caseBookList.enumerated() {
+        for (_, item) in caseBookList!.enumerated() {
             self.caseCardViewControllers.append(CaseCardViewController(caseBook: item))
         }
         
         pageViewController.setViewControllers([caseCardViewControllers[currentIndex]], direction: .forward, animated: true, completion: nil)
+        
+        button = UIButton(type: .system)
+        button!.backgroundColor = UIColor(red: 22 / 255, green: 24 / 255, blue: 35 / 255, alpha: 1)
+        button!.tintColor = UIColor(red: 233 / 255, green: 241 / 255, blue: 246 / 255, alpha: 1)
+        button!.setTitle("精彩呈现", for: .normal)
+        button!.layer.cornerRadius = 30
+        button!.layer.masksToBounds = true
+        self.view.addSubview(button!)
+        let ch = self.view.bounds.size.width * 0.8 * 7 / 5
+        let h = (self.view.bounds.size.height - ch) / 2 + ch
+        button!.snp.makeConstraints {
+            make in
+            make.width.equalTo(120)
+            make.height.equalTo(60)
+            make.top.equalToSuperview().offset(h + 20)
+            make.centerX.equalToSuperview()
+        }
+        button!.addTarget(self, action: #selector(handleNavigate), for: .touchUpInside)
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
@@ -58,6 +84,30 @@ class ViewController: UIViewController, UIPageViewControllerDelegate, UIPageView
             return self.caseCardViewControllers[index + 1]
         }
         return nil
+    }
+    
+    @objc func handleNavigate() {
+        if caseBookList![currentIndex].waiting {
+            let alert = UIAlertController(title: nil, message: "静候上线", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let incidentViewController = IncidentViewController(id: caseBookList![currentIndex].id)
+            incidentViewController.title = caseBookList![currentIndex].title
+            self.present(UINavigationController(rootViewController: incidentViewController), animated: true, completion: nil)
+        }
+    }
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            willTransitionTo pendingViewControllers: [UIViewController]) {
+        
+        let index = self.caseCardViewControllers.firstIndex(of: pendingViewControllers.first!)!
+        nextIndex = index
+    }
+    
+    func pageViewController(_: UIPageViewController, didFinishAnimating: Bool, previousViewControllers: [UIViewController], transitionCompleted: Bool) {
+        if (transitionCompleted) {
+            currentIndex = nextIndex
+        }
     }
     
 }
